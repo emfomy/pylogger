0633d9203a04c58865a7844fc24866093422ffd0
Mu Yang <emfomy@gmail.com>
2021-01-26 14:08:51 +0800

    Update.

--- logger/logger0.py
+++ logger/logger0.py
@@ -5,58 +5,73 @@ __author__ = 'Mu Yang <http://muyang.pro>'
 __copyright__ = 'Copyright 2020'
 
 __all__ = [
-    'demonstrate_logger',
-    'exceptstr',
-    'get_logger',
     'install_logger',
+    'demonstrate_logger',
 ]
 
+import builtins
+import inspect
 import logging
 
 import coloredlogs
 import verboselogs
 
-fmt = '%(asctime)s %(name)16.16s:%(lineno)-4d %(levelname)8s %(message)s'
-field_styles = {
-    'asctime': { 'color': 'green', 'faint': True, },
-    'name': { 'color': 'cyan', 'faint': True, },
-    'levelname': { 'color': 'black', 'bold': True, },
-}
-level_styles = {
-    'spam': { 'color': 'magenta', 'faint': True, },
-    'debug': { 'color': 'blue', },
-    'verbose': { 'color': 'magenta', },
-    'info': { 'color': 'cyan', },
-    'notice': { 'color': 'cyan', 'bold': True, },
-    'warning': { 'color': 'yellow', 'faint': True, },
-    'io': { 'color': 172, },
-    'success': { 'color': 'green', 'bold': True, },
-    'error': { 'color': 'red', 'bold': True, },
-    'critical': { 'background': 'red', 'bold': True, },
-}
+class Logger:
 
-def install_logger():
-    verboselogs.add_log_level(32, 'IO')
-    setattr(verboselogs.VerboseLogger, 'fin',   lambda self, name, *args, **kw: self._log(32, '<< '+name, args, **kw))
-    setattr(verboselogs.VerboseLogger, 'fout',  lambda self, name, *args, **kw: self._log(32, '>> '+name, args, **kw))
-    setattr(verboselogs.VerboseLogger, 'fcp',   lambda self, src, dst, *args, **kw: self._log(32, src+' => '+dst, args, **kw))
-    setattr(verboselogs.VerboseLogger, 'flink', lambda self, src, dst, *args, **kw: self._log(32, src+' <- '+dst, args, **kw))
+    fmt = '%(asctime)s %(name)24.24s:%(lineno)-4d %(levelname)8s %(message)s'
 
-    verboselogs.install()
-    coloredlogs.install(level=5, fmt=fmt, field_styles=field_styles, level_styles=level_styles)
+    field_styles = {
+        'asctime': { 'color': 'green', 'faint': True, },
+        'name': { 'color': 'cyan', 'faint': True, },
+        'levelname': { 'color': 'black', 'bold': True, },
+    }
 
-def get_logger(modname):
-    modname = modname[-16:]
-    return logging.getLogger(modname)
+    level_styles = {
+        'spam': { 'color': 'magenta', 'faint': True, },
+        'debug': { 'color': 'blue', },
+        'verbose': { 'color': 'magenta', },
+        'info': { 'color': 'cyan', },
+        'notice': { 'color': 'cyan', 'bold': True, },
+        'warning': { 'color': 'yellow', 'bold': True, },
+        'io': { 'color': 172, , },
+        'success': { 'color': 'green', 'bold': True, },
+        'error': { 'color': 'red', 'bold': True, },
+        'critical': { 'background': 'red', 'bold': True, },
+    }
 
-def exceptstr(e):
-    return f'{e.__class__.__name__}: {e}'
+    @classmethod
+    def install_logger(cls):
+        verboselogs.add_log_level(32, 'IO')
+        setattr(verboselogs.VerboseLogger, 'fin',   lambda self, name, *args, **kw: self._log(32, '<< '+name, args, **kw))
+        setattr(verboselogs.VerboseLogger, 'fout',  lambda self, name, *args, **kw: self._log(32, '>> '+name, args, **kw))
+        setattr(verboselogs.VerboseLogger, 'fcp',   lambda self, src, dst, *args, **kw: self._log(32, src+' => '+dst, args, **kw))
+        setattr(verboselogs.VerboseLogger, 'flink', lambda self, src, dst, *args, **kw: self._log(32, src+' <- '+dst, args, **kw))
 
-def demonstrate_logger(logger = None):
-    if not logger:
-        logger = get_logger(__name__)
-    for name in level_styles:
-        level = coloredlogs.level_to_number(name)
-        logger.log(level, f'message with level {name} ({level})')
+        verboselogs.install()
+        coloredlogs.install(level=5, fmt=cls.fmt, field_styles=cls.field_styles, level_styles=cls.level_styles)
+        builtins.logger = cls.logger
+        builtins.exceptstr = cls.exceptstr
 
-install_logger()
+    @staticmethod
+    def logger():
+        frm = inspect.stack()[1]
+        mod = inspect.getmodule(frm[0])
+        name = getattr(mod, '__name__', '')
+        return logging.getLogger(name)
+
+    @staticmethod
+    def exceptstr(e):
+        return f'{e.__class__.__name__}: {e}'
+
+    @classmethod
+    def demonstrate_logger(cls):
+        for name in cls.level_styles:
+            level = coloredlogs.level_to_number(name)
+            logger().log(level, f'message with level {name} ({level})')
+
+install_logger = Logger.install_logger
+demonstrate_logger = Logger.demonstrate_logger
+
+if __name__ == '__main__':
+    install_logger()
+    demonstrate_logger()
--- logger/logger1.py
+++ logger/logger1.py
@@ -6,11 +6,11 @@ __copyright__ = 'Copyright 2020'
 
 __all__ = [
     'install_logger',
+    'get_logger',
+    'exceptstr',
     'demonstrate_logger',
 ]
 
-import builtins
-import inspect
 import logging
 
 import coloredlogs
@@ -18,22 +18,20 @@ import verboselogs
 
 class Logger:
 
-    fmt = '%(asctime)s %(name)24.24s:%(lineno)-4d %(levelname)8s %(message)s'
-
+    fmt = '%(asctime)s %(name)16.16s:%(lineno)-4d %(levelname)8s %(message)s'
     field_styles = {
         'asctime': { 'color': 'green', 'faint': True, },
         'name': { 'color': 'cyan', 'faint': True, },
         'levelname': { 'color': 'black', 'bold': True, },
     }
-
     level_styles = {
         'spam': { 'color': 'magenta', 'faint': True, },
         'debug': { 'color': 'blue', },
         'verbose': { 'color': 'magenta', },
         'info': { 'color': 'cyan', },
         'notice': { 'color': 'cyan', 'bold': True, },
-        'warning': { 'color': 'yellow', 'bold': True, },
-        'io': { 'color': 172, , },
+        'warning': { 'color': 'yellow', 'faint': True, },
+        'io': { 'color': 172, },
         'success': { 'color': 'green', 'bold': True, },
         'error': { 'color': 'red', 'bold': True, },
         'critical': { 'background': 'red', 'bold': True, },
@@ -49,27 +47,27 @@ class Logger:
 
         verboselogs.install()
         coloredlogs.install(level=5, fmt=cls.fmt, field_styles=cls.field_styles, level_styles=cls.level_styles)
-        builtins.logger = cls.logger
-        builtins.exceptstr = cls.exceptstr
 
     @staticmethod
-    def logger():
-        frm = inspect.stack()[1]
-        mod = inspect.getmodule(frm[0])
-        name = getattr(mod, '__name__', '')
-        return logging.getLogger(name)
+    def get_logger(modname):
+        modname = modname[-16:]
+        return logging.getLogger(modname)
 
     @staticmethod
     def exceptstr(e):
         return f'{e.__class__.__name__}: {e}'
 
     @classmethod
-    def demonstrate_logger(cls):
+    def demonstrate_logger(cls, logger=None):
+        if not logger:
+            logger = get_logger(__name__)
         for name in cls.level_styles:
             level = coloredlogs.level_to_number(name)
-            logger().log(level, f'message with level {name} ({level})')
+            logger.log(level, f'message with level {name} ({level})')
 
 install_logger = Logger.install_logger
+get_logger = Logger.get_logger
+exceptstr = Logger.exceptstr
 demonstrate_logger = Logger.demonstrate_logger
 
 if __name__ == '__main__':
